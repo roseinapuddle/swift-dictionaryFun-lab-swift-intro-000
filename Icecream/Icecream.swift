3879becd9f5c37388fadf3d4959e7eafd83fecab
Brendt Bly <brendtbly@Brendts-MacBook-Pro.local>
2017-02-15 13:12:33 -0600

    Tests passed

--- Icecream/Icecream.swift
+++ Icecream/Icecream.swift
@@ -9,78 +9,123 @@
 class Icecream {
     
     // 1. Create the Dictionary here. The name of the variable should be favoriteFlavorsOfIceCream
-    
-    
-    
+    var favoriteFlavorsOfIceCream: [String:String] = ["Joe" : "Peanut Butter and Chocolate",
+                                                      "Tim" : "Natural Vanilla",
+                                                      "Sophie" : "Mexican Chocolate",
+                                                      "Deniz" : "Natural Vanilla",
+                                                      "Tom" : "Mexican Chocolate",
+                                                      "Jim" : "Natural Vanilla",
+                                                      "Susan" : "Cookies 'N' Cream"]
+    /*  
+     "Deniz likes Natural Vanilla\n
+     Jim likes Natural Vanilla\nJoe likes Peanut Butter and Chocolate\n
+     Sophie likes Mexican Chocolate\n
+     Susan likes Cookies 'N' Cream\n
+     Tim likes Natural Vanilla\n
+     Tom likes Mexican Chocolate"
+     
+     Joe: Peanut Butter and Chocolate
+ Tim: Natural Vanilla
+ Sophie: Mexican Chocolate
+ Deniz: Natural Vanilla
+ Tom: Mexican Chocolate
+ Jim: Natural Vanilla
+ Susan: Cookies 'N' Cream */
+ 
     
     
     // 2.
+    func names(forFlavor flavor: String) -> [String] {
+        var names: [String] = []
+        for (name, chosenFlavor) in favoriteFlavorsOfIceCream {
+            if flavor == chosenFlavor {
+                names.append(name)
+            }
+        }
+        return names
+    }
     
-    
-    
+   
+
     
     
     
     // 3.
     
-    
-    
+    func count(forFlavor flavor: String) -> Int {
+      
+        let namesPeople = names(forFlavor: flavor)
+        let numPeople = namesPeople.count
+        return numPeople
+    }
     
     
     
     
     // 4.
-   
     
+    func flavor(forPerson person: String) -> String? {
+        if let flavor = favoriteFlavorsOfIceCream[person] {
+            return flavor
+        } else {
+            return nil
+        }
+    }
     
-    
-    
-    
-    
-    // 5.
-   
-    
-    
-    
-    
-    
+    // 5. 
+    func replace(flavor: String, forPerson: String) -> Bool {
+        if let _ = favoriteFlavorsOfIceCream[forPerson] {
+            favoriteFlavorsOfIceCream[forPerson] = flavor
+            return true
+        } else {
+            return false
+        }
+    }
     
     // 6.
-    
-    
-    
-    
-    
-    
-    
+    func remove(person: String) -> Bool {
+        if let _ = favoriteFlavorsOfIceCream[person] {
+            favoriteFlavorsOfIceCream[person] = nil
+            return true
+        } else {
+            return false
+        }
+    }
     
     // 7.
-    
-    
-    
-    
-    
-    
-    
+    func numberOfAttendees() -> Int {
+        let attendees = favoriteFlavorsOfIceCream.count
+        return attendees
+    }
     
     // 8.
+    func add(person: String, withFlavor: String) -> Bool {
+        if favoriteFlavorsOfIceCream[person] == nil {
+            favoriteFlavorsOfIceCream[person] = withFlavor
+            return true
+        } else {
+            return false
+        }
+    }
     
+    //9. 
     
-    
-    
-    
-    
-    
-    
-    
-    // 9.
-    
-    
-    
-    
-    
-    
-    
+    func attendeeList() -> String {
+        let allNames = Array(favoriteFlavorsOfIceCream.keys).sorted()
+        var sentence = "\(allNames[0]) likes \(favoriteFlavorsOfIceCream[allNames[0]]!)"
+        for i in 1...(allNames.count - 1) {
+            sentence += "\n\(allNames[i]) likes \(favoriteFlavorsOfIceCream[allNames[i]]!)"
+        }
+        return sentence
+    }
+    /*
+          9.
+     If Susan's notes and memory are correct, there are now 7 people coming to the party. She'd like you to write a function, attendeeList(), which will create a list "Name likes Ice Cream", one attendee per line in alphabetical order, just so she can make sure she remembers correctly.
+     The function should be called attendeelist(). The return type should be String.
+     A hint to help you produce a list in alphabetical order:
+     let allNames = Array(favoriteFlavorsOfIceCream.keys).sorted()
+     allNames is an array of String's, placing all the names in alphabetical order. Use this to your advantage. Make sure the last part of your String doesn't end with a newline character \n.
+     */
     
 
-}
+    }
--- Icecream/ViewController.swift
+++ Icecream/ViewController.swift
@@ -19,9 +19,21 @@ class ViewController: UIViewController {
         // Test your functions here. You have access to the icecream constant created above.
         
         
-        
-        
-        
+/* 1.
+ Some of the Flatiron School instructors are putting on a summer ice cream party. Susan, one of the instructors, has collected some information on everyone's favorite ice cream flavors. She only wants to make one trip to the grocery store, though, so she needs you to write some functions to remind her what ice cream she needs to buy when she goes.
+ She asked everyone attending the party what their favorite flavor of ice cream is. Here are the people attending the ice cream party, and what their favorite flavors of ice cream are:
+ Joe: Peanut Butter and Chocolate
+ Tim: Natural Vanilla
+ Sophie: Mexican Chocolate
+ Deniz: Natural Vanilla
+ Tom: Mexican Chocolate
+ Jim: Natural Vanilla
+ Susan: Cookies 'N' Cream
+ In order to help Susan, locate the Icecream.swift file and look for the following comment:
+ // 1. Create the Dictionary here. The name of the variable should be favoriteFlavorsOfIceCream
+ Right below this comment, you should create a dictionary of type [String : String] and assign a default value to the variable. The keys of this dictionary should be the names and the values associated with each of those keys should be that individuals favorite ice-cream.
+ In the IcecreamTests.swift file, you should be able to pass the test listed under // 1. before moving on!
+ */        
         
         
     }
